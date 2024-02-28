170d128a7d1a299de6104218260e1533909fe6bc
Madelline Araya <119256205+MadeAraya@users.noreply.github.com>
2024-02-28 15:23:07 -0600

    Commit de Camila Corrales
    Creación de index y plantilla

---
+++ src/test/java/com/proyectoweb/ProyectoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
